7ee3f0b3caffd40c48437ad01055d5c920cac987
Katty Ptk <46168818+katty-ptk@users.noreply.github.com>
2024-03-09 14:46:45 +0200

    Checkbarcode (#4)
    * am facut loginscreenul frumos
    
    * check if barcode has already been scanned
    
    * merged branches
    
    ---------
    
    Co-authored-by: Stan Georgian <stangeorgian38@gmail.com>
    Co-authored-by: katty-ptk <katy.lazar05@g,ail.com>

--- lib/screens/login.screen.dart
+++ lib/screens/login.screen.dart
@@ -1,36 +1,135 @@
 import 'package:flutter/material.dart';
-import 'package:recyclecash/screens/home.screen.dart';
-import 'package:recyclecash/services/firestore.service.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class LoginScreen extends StatefulWidget {
-  const LoginScreen({super.key});
+import '../services/firestore.service.dart';
+import 'home.screen.dart';
 
-  @override
-  State<LoginScreen> createState() => _LoginScreenState();
-}
-
-class _LoginScreenState extends State<LoginScreen> {
-  Future<void> login() async {
-    final SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-
-    FirestoreService().getUserName();
-
-    Navigator.pushReplacement(context, MaterialPageRoute(builder: (BuildContext context) => HomeScreen()));
-
-  }
+class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(onPressed: () async => await login(), child: Text('login as test'))
-          ],
+
+    Future<void> login() async {
+      FirestoreService().getUserName();
+
+      Navigator.pushReplacement(context,
+          MaterialPageRoute(builder: (BuildContext context) => HomeScreen()));
+    }
+
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/SL-051919-20420-09.jpg"),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Container(
+            margin: EdgeInsets.all(24),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                _header(context),
+                _inputField(context, login),
+                _forgotPassword(context),
+                _signup(context),
+              ],
+            ),
+          ),
         ),
       ),
+    );
+  }
+
+  _header(context) {
+    return Row(
+      children: [
+        Image.asset(
+          "assets/logo.png", // Replace with your image asset path
+          height: 100, // Adjust the height as needed
+        ),
+        SizedBox(width: 10), // Add some space between the logo and text
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "  Welcome to",
+              style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold, color: Color(0xFF36A383)),
+            ),
+            Text(
+              "RecycleCash",
+              style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold, color: Color(0xFF36A383)),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  _inputField(BuildContext context, Function login ) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        TextField(
+          decoration: InputDecoration(
+            hintText: "Username",
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(18),
+              borderSide: BorderSide.none,
+            ),
+            fillColor: Color(0xFF36A383).withOpacity(0.1), // Changed box color
+            filled: true,
+            prefixIcon: Icon(Icons.person, color: Color(0xFF595959)),
+          ),
+        ),
+        SizedBox(height: 20),
+        TextField(
+          decoration: InputDecoration(
+            hintText: "Password",
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(18),
+              borderSide: BorderSide.none,
+            ),
+            fillColor: Color(0xFF36A383).withOpacity(0.1), // Changed box color
+            filled: true,
+            prefixIcon: Icon(Icons.lock, color: Color(0xFF595959)),
+          ),
+          obscureText: true,
+        ),
+        SizedBox(height: 20),
+        ElevatedButton(
+          onPressed: () => login(),
+          style: ElevatedButton.styleFrom(
+            shape: StadiumBorder(),
+            padding: EdgeInsets.symmetric(vertical: 16),
+          ),
+          child: Text(
+            "Login",
+            style: TextStyle(fontSize: 20, color: Color(0xFF595959)),
+          ),
+        )
+      ],
+    );
+  }
+
+  _forgotPassword(context) {
+    return TextButton(
+      onPressed: () {},
+      child: Text(
+        "Forgot password?",
+        style: TextStyle(fontSize: 15, color: Color(0xFF595959)),
+      ),
+    );
+  }
+
+  _signup(context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text("Don't have an account? ", style: TextStyle(color: Color(0xFF595959))), // Added TextStyle for text color
+        TextButton(onPressed: () {}, child: Text("Sign Up", style: TextStyle(color: Color(0xFF595959)))), // Added TextStyle for text color
+      ],
     );
   }
 }
