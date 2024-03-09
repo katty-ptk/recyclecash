618ec7246f55aacb61b87561967a64daebd2a027
katty-ptk <katy.lazar05@g,ail.com>
2024-03-09 02:10:10 +0200

    functions

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,43 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:recyclecash/firebase_options.dart';
 import 'package:recyclecash/screens/home.screen.dart';
+import 'package:recyclecash/screens/login.screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
+
+  bool isLoggedIn;
+  final SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+
+  if ( sharedPreferences.getString('tickets') != null ) {
+    isLoggedIn = true;
+  } else {
+    isLoggedIn = false;
+  }
+
+  runApp(MyApp(isLoggedIn: isLoggedIn,));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
-  // This widget is the root of your application.
+  final bool isLoggedIn;
+  const MyApp({super.key, required this.isLoggedIn});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomeScreen()
+      home: isLoggedIn == true ? const HomeScreen() : const LoginScreen()
     );
   }
 }
---
+++ lib/services/firestore.service.dart
@@ -0,0 +1,127 @@
+import 'dart:math';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class FirestoreService {
+  final db = FirebaseFirestore.instance;
+
+  Future<String> getUserName() async {
+    DocumentReference<Map<String, dynamic>> userDoc =
+    db.collection('users').doc('Osbm2OzwhYVd5fPFsWnHudTPezX2');
+
+    final SharedPreferences sharedPreferences =
+    await SharedPreferences.getInstance();
+    String userName = '';
+
+    try {
+      final user = await userDoc.get();
+      final userData = user.data();
+
+      await sharedPreferences.setString('userName', userData?['name']);
+
+      userName = userData?['name'];
+    } catch (error) {
+      print(error);
+    }
+
+    return userName;
+  }
+
+  Future<List<Set<String>>> getUserTickets() async {
+    final SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+
+    DocumentReference<Map<String, dynamic>> userDoc =
+    db.collection('users').doc('Osbm2OzwhYVd5fPFsWnHudTPezX2');
+
+    List<Set<String>> userTickets = [];
+
+    try {
+      final user = await userDoc.get();
+      final userData = user.data();
+
+      userData?.entries.forEach((entry) {
+        if ( entry.key != 'name' ) {
+          userTickets.add({
+            entry.key,
+            entry.value
+          });
+        }
+      });
+
+      await sharedPreferences.setStringList('tickets', userData?['tickets']);
+    } catch (error) {
+      print(error);
+    }
+
+    print(userTickets);
+
+    return userTickets;
+  }
+
+  Future<void> generateBarcode(String storeName, String originalBarcode, String price ) async {
+    int originalPrice = int.parse(getPriceFromBarcode(originalBarcode));
+
+    String suma = ( originalPrice + int.parse(price) ).toString();
+    print("original price: $originalPrice");
+    print("new price: ${int.parse(price)}");
+    print("sum: ${int.parse(suma)}");
+
+    String newBarcode = '';
+
+    if ( storeName == 'penny' ) {
+      newBarcode = '2263000450910019200${suma}00';
+    } else if ( storeName == 'lidl') {
+      newBarcode = '2010${suma}2000000264203810';
+    }
+
+    DocumentReference<Map<String, dynamic>> userDoc =
+    db.collection('users').doc('Osbm2OzwhYVd5fPFsWnHudTPezX2');
+
+    try {
+      userDoc.update({
+        storeName: newBarcode
+      });
+    } catch (error) {
+      print(error);
+    }
+  }
+
+  String getPriceFromBarcode( String barcode ) {
+    String suma = '';
+    String prefix = barcode.substring(0, 4);
+
+    if ( prefix == '2010' ) {  // lidl
+      suma = barcode.substring(5, 9);
+    } else {
+      barcode = barcode.substring(0, barcode.length - 2);
+      suma = barcode.substring(barcode.length - 4);
+    }
+
+    return suma;
+  }
+
+  Future<bool> checkIfBarcodeWasAlreadyUsed(String storeName, String barcode) async{
+    bool barcodeAlreadyUsed = false;
+
+    DocumentReference<Map<String, dynamic>> storeDoc =
+    db.collection('stores').doc(storeName);
+
+    try {
+      final store = await storeDoc.get();
+      final storeData = store.data();
+
+      storeData?.entries.forEach((entry) {
+        if ( entry.value == barcode ) {
+         barcodeAlreadyUsed = true;
+        }
+      });
+    } catch (error) {
+      print(error);
+    }
+
+    return barcodeAlreadyUsed;
+  }
+
+}
