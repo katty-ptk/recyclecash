0ee0d49047ea27a31b7dfc86d9bcf00799996382
Stan Georgian <stangeorgian38@gmail.com>
2024-03-09 15:27:07 +0200

    login function

--- lib/screens/login.screen.dart
+++ lib/screens/login.screen.dart
@@ -1,17 +1,9 @@
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    double screenHeight = MediaQuery.of(context).size.height;
-    double screenWidth = MediaQuery.of(context).size.width;
-import 'package:shared_preferences/shared_preferences.dart';
-
 import '../services/firestore.service.dart';
 import 'home.screen.dart';
 
 class LoginScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
 
@@ -22,7 +14,7 @@ class LoginScreen extends StatelessWidget {
           MaterialPageRoute(builder: (BuildContext context) => HomeScreen()));
     }
 
-    return SafeArea(
+      return SafeArea(
       child: Scaffold(
         body: Container(
           decoration: BoxDecoration(
@@ -32,14 +24,14 @@ class LoginScreen extends StatelessWidget {
             ),
           ),
           child: Container(
-            margin: EdgeInsets.all(screenWidth * 0.05),
+            margin: EdgeInsets.all(24),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                _header(context, screenWidth),
-                _inputField(context, screenWidth),
-                _forgotPassword(context, screenWidth),
-                _signup(context, screenWidth),
+                _header(context),
+                _inputField(context, login),
+                _forgotPassword(context),
+                _signup(context),
               ],
             ),
           ),
@@ -48,26 +40,24 @@ class LoginScreen extends StatelessWidget {
     );
   }
 
-  _header(context, double screenWidth) {
+  _header(context) {
     return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Image.asset(
-          "assets/logo.png",
-          height: screenWidth * 0.2,
+          "assets/logo.png", // Replace with your image asset path
+          height: 100, // Adjust the height as needed
         ),
-        SizedBox(width: screenWidth * 0.03),
-
+        SizedBox(width: 10), // Add some space between the logo and text
         Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
-              "   Welcome to",
-              style: TextStyle(fontSize: screenWidth * 0.06, fontWeight: FontWeight.bold, color: Color(0xFF36A383)),
+              "  Welcome to",
+              style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold, color: Color(0xFF36A383)),
             ),
             Text(
               "RecycleCash",
-              style: TextStyle(fontSize: screenWidth * 0.075, fontWeight: FontWeight.bold, color: Color(0xFF36A383)),
+              style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold, color: Color(0xFF36A383)),
             ),
           ],
         ),
@@ -75,8 +65,7 @@ class LoginScreen extends StatelessWidget {
     );
   }
 
-  _inputField(context, double screenWidth) {
-
+  _inputField(BuildContext context, Function login) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
@@ -84,61 +73,62 @@ class LoginScreen extends StatelessWidget {
           decoration: InputDecoration(
             hintText: "Username",
             border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(14), // Adjusted to screen width proportion
+              borderRadius: BorderRadius.circular(18),
               borderSide: BorderSide.none,
             ),
-            fillColor: Color(0xFF36A383).withOpacity(0.1),
+            fillColor: Color(0xFF36A383).withOpacity(0.1), // Changed box color
             filled: true,
-            contentPadding: EdgeInsets.symmetric(vertical: screenWidth * 0.032), // Adjusted to screen width proportion
             prefixIcon: Icon(Icons.person, color: Color(0xFF595959)),
           ),
         ),
+        SizedBox(height: 20),
         TextField(
           decoration: InputDecoration(
             hintText: "Password",
             border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(14), // Adjusted to screen width proportion
+              borderRadius: BorderRadius.circular(18),
               borderSide: BorderSide.none,
             ),
-            fillColor: Color(0xFF36A383).withOpacity(0.1),
+            fillColor: Color(0xFF36A383).withOpacity(0.1), // Changed box color
             filled: true,
-            contentPadding: EdgeInsets.symmetric(vertical: screenWidth * 0.032), // Adjusted to screen width proportion
             prefixIcon: Icon(Icons.lock, color: Color(0xFF595959)),
           ),
           obscureText: true,
         ),
-        SizedBox(height: screenWidth * 0.04),
+        SizedBox(height: 20),
         ElevatedButton(
-          onPressed: () {},
+          onPressed: () {
+            login();
+          },
           style: ElevatedButton.styleFrom(
             shape: StadiumBorder(),
-            padding: EdgeInsets.symmetric(vertical: screenWidth * 0.04),
+            padding: EdgeInsets.symmetric(vertical: 16),
           ),
           child: Text(
             "Login",
-            style: TextStyle(fontSize: screenWidth * 0.04, color: Color(0xFF595959)),
+            style: TextStyle(fontSize: 20, color: Color(0xFF595959)),
           ),
         )
       ],
     );
   }
 
-  _forgotPassword(context, double screenWidth) {
+  _forgotPassword(context) {
     return TextButton(
       onPressed: () {},
       child: Text(
         "Forgot password?",
-        style: TextStyle(fontSize: screenWidth * 0.04, color: Color(0xFF595959)),
+        style: TextStyle(fontSize: 15, color: Color(0xFF595959)),
       ),
     );
   }
 
-  _signup(context, double screenWidth) {
+  _signup(context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Text("Don't have an account? ", style: TextStyle(fontSize: screenWidth * 0.04, color: Color(0xFF595959))),
-        TextButton(onPressed: () {}, child: Text("Sign Up", style: TextStyle(fontSize: screenWidth * 0.04, color: Color(0xFF595959)))),
+        Text("Don't have an account? ", style: TextStyle(color: Color(0xFF595959))), // Added TextStyle for text color
+        TextButton(onPressed: () {}, child: Text("Sign Up", style: TextStyle(color: Color(0xFF595959)))), // Added TextStyle for text color
       ],
     );
   }
